87c36fbf8a9558937ad1e211604f2471feb7f6a3
SebastianGaud <sebastiano.gaudeano@live.com>
2019-06-18 14:39:16 +0200

    solution to sum of multiples exercise

--- sum-of-multiples/SumOfMultiples.fs
+++ sum-of-multiples/SumOfMultiples.fs
@@ -1,3 +1,11 @@
 ﻿module SumOfMultiples
+open System.Collections.Generic
 
-let sum (numbers: int list) (upperBound: int): int = failwith "You need to implement this function."
+let divisibleBy number divisor = (divisor <> 0) && number % divisor = 0;
+
+let filterByDivisible (numbers: int list) (num: int) = 
+    numbers |> List.exists (fun x -> divisibleBy num x)
+
+
+let sum (numbers: int list) (upperBound: int): int = 
+    [1 .. upperBound - 1] |> List.filter (filterByDivisible numbers)  |> List.sum
