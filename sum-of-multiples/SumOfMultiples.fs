0205f80245ec82aa19349d6bfd7c7d4d5390dc5e
SebastianGaud <sebastiano.gaudeano@live.com>
2019-06-18 14:39:46 +0200

    clean up

--- sum-of-multiples/SumOfMultiples.fs
+++ sum-of-multiples/SumOfMultiples.fs
@@ -1,11 +1,9 @@
 ﻿module SumOfMultiples
-open System.Collections.Generic
 
 let divisibleBy number divisor = (divisor <> 0) && number % divisor = 0;
 
 let filterByDivisible (numbers: int list) (num: int) = 
     numbers |> List.exists (fun x -> divisibleBy num x)
-
 
 let sum (numbers: int list) (upperBound: int): int = 
     [1 .. upperBound - 1] |> List.filter (filterByDivisible numbers)  |> List.sum
